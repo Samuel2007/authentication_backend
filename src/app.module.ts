da65e37dc79412eee173cfcaec7d4e17a68d34a1
samuel <samuel.cwiek@wp.pl>
2025-12-16 19:05:20 +0100

    added database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config as dotenvConfig } from 'dotenv';
+dotenvConfig();
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    MongooseModule.forRoot(process.env.DB_HOST || ''),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config as dotenvConfig } from 'dotenv';
-
-// Load environment variables from .env into process.env
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 dotenvConfig();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+  app.enableCors({
+    origin: 'http://localhost:3000', // adres frontendu
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // tylko jeśli używasz cookies / auth
+  });
   await app.listen(port);
   console.log(`Listening on port ${port}`);
 }
